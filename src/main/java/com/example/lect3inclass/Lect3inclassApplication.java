4b1e7b7b7cd57da503431bd1abfdffd54f9f9521
LHL <louisehjulerlarsen@gmail.com>
2024-09-13 09:47:19 +0200

    sprint init

---
+++ src/main/java/com/example/lect3inclass/Lect3inclassApplication.java
@@ -0,0 +1,13 @@
+package com.example.lect3inclass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lect3inclassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lect3inclassApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/lect3inclass/Lect3inclassApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lect3inclass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lect3inclassApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
